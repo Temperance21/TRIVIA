5467a6a193467c1fd34aa9835deb1fd25921d5a0
Asia Garcia <131497952+Temperance21@users.noreply.github.com>
2023-05-27 17:43:17 -0400

    Update js.js

--- js.js
+++ js.js
@@ -34,8 +34,7 @@ const Questions = [{
  
 // Set start
 var start = true;
-var selectedOption;
-
+ 
 // Iterate
 function iterate(id) {
  
@@ -46,58 +45,109 @@ function iterate(id) {
     // Getting the question
     const question = document.getElementById("question");
  
+ 
     // Setting the question text
     question.innerText = Questions[id].q;
  
     // Getting the options
-    const options = [
-        document.getElementById('op1'),
-        document.getElementById('op2'),
-        document.getElementById('op3'),
-        document.getElementById('op4')
-    ];
+    const op1 = document.getElementById('op1');
+    const op2 = document.getElementById('op2');
+    const op3 = document.getElementById('op3');
+    const op4 = document.getElementById('op4');
  
-    // Providing option text and value
-    options.forEach((option, index) => {
-        option.innerText = Questions[id].a[index].text;
-        option.value = Questions[id].a[index].isCorrect;
-        option.style.backgroundColor = "Slategrey";
-    });
  
-    // Show selection
-    options.forEach((option, index) => {
-        option.addEventListener("click", () => {
-            options.forEach((opt, idx) => {
-                if (index === idx) {
-                    opt.style.backgroundColor = "creamyyellow";
-                    selectedOption = opt;
-                } else {
-                    opt.style.backgroundColor = "Slategrey";
-                }
-            });
-        });
-    });
+    // Providing option text
+    op1.innerText = Questions[id].a[0].text;
+    op2.innerText = Questions[id].a[1].text;
+    op3.innerText = Questions[id].a[2].text;
+    op4.innerText = Questions[id].a[3].text;
+ 
+    // Providing the true or false value to the options
+    op1.value = Questions[id].a[0].isCorrect;
+    op2.value = Questions[id].a[1].isCorrect;
+    op3.value = Questions[id].a[2].isCorrect;
+    op4.value = Questions[id].a[3].isCorrect;
+ 
+    var selected = "";
+ 
+    // Show selection for op1
+    op1.addEventListener("click", () => {
+        op1.style.backgroundColor = "creamyyellow";
+        op2.style.backgroundColor = "Midnightblue";
+        op3.style.backgroundColor = "Slategrey";
+        op4.style.backgroundColor = "Slategrey";
+        selected = op1.value;
+    })
+ 
+    // Show selection for op2
+    op2.addEventListener("click", () => {
+        op1.style.backgroundColor = "Slategrey";
+        op2.style.backgroundColor = "Creamyblue";
+        op3.style.backgroundColor = "Slategrey";
+        op4.style.backgroundColor = "Slategrey";
+        selected = op2.value;
+    })
+ 
+    // Show selection for op3
+    op3.addEventListener("click", () => {
+        op1.style.backgroundColor = "Slategrey";
+        op2.style.backgroundColor = "Slategrey";
+        op3.style.backgroundColor = "creamyyellow";
+        op4.style.backgroundColor = "Slategrey";
+        selected = op3.value;
+    })
+ 
+    // Show selection for op4
+    op4.addEventListener("click", () => {
+        op1.style.backgroundColor = "Slategrey";
+        op2.style.backgroundColor = "Slategrey";
+        op3.style.backgroundColor = "Midnightblue";
+        op4.style.backgroundColor = "Slategrey";
+        selected = op4.value;
+    })
+ 
+    function clearOptionSelections() {
+        op1.style.backgroundColor = "Slategrey";  //color for unselected options
+        op2.style.backgroundColor = "Slategrey";
+        op3.style.backgroundColor = "Slategrey";
+        op4.style.backgroundColor = "Slategrey";
+    }
+    
+    // Next button and method
+    const next = document.getElementsByClassName('next')[0];
+    var id = 0;
+    
+    next.addEventListener("click", () => {
+        start = false;
+        if (id < 2) {
+            id++;
+            iterate(id);
+            clearOptionSelections(); // Clears option selections when moving to a new question
+            console.log(id);
+        }
+    })
+
+
 
     // Grabbing the evaluate button
     const evaluate = document.getElementsByClassName("evaluate");
  
     // Evaluate method
     evaluate[0].addEventListener("click", () => {
-        if (selectedOption.value === "true") {
-            selectedOption.style.backgroundColor = "MidnightBlue";
+        if (selected == "true") {
             result[0].innerHTML = "True";
-            result[0].style.color = "Green";
+            result[0].style.color = "Yellow";
             result[0].style.fontSize = "larger";
         } else {
             result[0].innerHTML = "False";
-            result[0].style.color = "Red";
+            result[0].style.color = "red";
             result[0].style.fontSize = "larger";
         }
     })
 }
  
 if (start) {
-    iterate(0);
+    iterate("0");
 }
  
 // Next button and method
@@ -105,4 +155,11 @@ const next = document.getElementsByClassName('next')[0];
 var id = 0;
  
 next.addEventListener("click", () => {
-    start
+    start = false;
+    if (id < 2) {
+        id++;
+        iterate(id);
+        console.log(id);
+    }
+ 
+})
